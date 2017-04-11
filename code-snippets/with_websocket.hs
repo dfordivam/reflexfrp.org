dc82e1d11430ce589a5b9ea168f6a478adf493c4
Divam <dfordivam@gmail.com>
2017-04-11 11:28:22 +0900

    Code cleanup

--- code-snippets/with_websocket.hs
+++ code-snippets/with_websocket.hs
@@ -27,8 +27,10 @@ class (DomBuilder t m)
   getResponse :: (ToJSON req, FromJSON resp)
     => Event t req -> m (Event t resp)
 
-enc :: (ToJSON a, Functor f) => f a -> f [ByteString]
-enc mes = (:[]) <$> encode <$> mes
+newtype WithWebSocketT t m a =
+  WithWebSocketT
+  { unWithWebSocketT :: RWST (Event t ByteString) (Event t [ByteString]) () m a}
+  deriving (Functor, Applicative, Monad, MonadFix, MonadIO, MonadTrans)
 
 instance (DomBuilder t m) => WithWebSocket t (WithWebSocketT t m) where
   getResponse req = do
@@ -38,21 +40,11 @@ instance (DomBuilder t m) => WithWebSocket t (WithWebSocketT t m) where
     -- Provide the response
     ev <- ask
     return $ fforMaybe ev decode
-  -- merge implementation is simple
-  -- Just call encode and get bytestring
-
--- type WithWebSocket t m = MonadRWS (Event t ByteString) (Event t [ByteString]) () m
- -- (RWST (Event t ByteString) (Event t [ByteString]) () m)
 
 instance (Reflex t) => MonadTransControl (WithWebSocketT t) where
   type StT (WithWebSocketT t) a = StT (RWST (Event t ByteString) (Event t [ByteString]) ()) a
   liftWith = defaultLiftWith WithWebSocketT unWithWebSocketT
   restoreT = defaultRestoreT WithWebSocketT
-
-newtype WithWebSocketT t m a =
-  WithWebSocketT
-  { unWithWebSocketT :: RWST (Event t ByteString) (Event t [ByteString]) () m a}
-  deriving (Functor, Applicative, Monad, MonadFix, MonadIO, MonadTrans)
 
 instance (Reflex t, Monad m) =>
   MonadReader (Event t ByteString) (WithWebSocketT t m)
@@ -60,59 +52,29 @@ instance (Reflex t, Monad m) =>
   MonadWriter (Event t [ByteString]) (WithWebSocketT t m)
 instance DomBuilder t m => DomBuilder t (WithWebSocketT t m) where
   type DomBuilderSpace (WithWebSocketT t m) = DomBuilderSpace m
-  -- {-# INLINABLE textNode #-}
-  -- textNode cfg = lift $ textNode cfg
+  {-# INLINABLE element #-}
   element t cfg child = do
     (ret, w) <- liftWith $ \run -> do
-      let myEl r = do
-            (a,_,w) <- r
-            ret <- element t (liftElementConfig cfg) (pure a)
-            return (ret, w)
-      myEl (run child)
+      (a,_,w) <- run child
+      ret <- element t (liftElementConfig cfg) (pure a)
+      return (ret, w)
     tell w
     return ret
 
-      -- let myRun b = do
-      --       (a,_, w) <- run b
-      --       return a
-      -- element t (liftElementConfig cfg) $ myRun child
-  {-# INLINABLE element #-}
-  -- {-# INLINABLE element #-}
-  -- element t cfg child = lift $ element t cfg $ run
-  --   --liftWith $ \run -> element t cfg $ run child
-  -- {-# INLINABLE inputElement #-}
-  -- inputElement cfg = lift $ inputElement cfg
-  -- {-# INLINABLE textAreaElement #-}
-  -- textAreaElement cfg = lift $ textAreaElement cfg
   {-# INLINABLE selectElement #-}
   selectElement cfg child = do
     (ret, w) <- liftWith $ \run -> do
-      let myEl r = do
-            (a,_,w) <- r
-            ret <- selectElement cfg' (pure a)
-            return (ret, w)
-          cfg' = cfg
+      let cfg' = cfg
                   { _selectElementConfig_elementConfig =
                     liftElementConfig $ _selectElementConfig_elementConfig cfg
                   }
-      myEl (run child)
+      (a,_,w) <- run child
+      ret <- selectElement cfg' (pure a)
+      return (ret, w)
     tell w
     return ret
 
 instance (MonadAdjust t m) => MonadAdjust t (WithWebSocketT t m)
-  -- inputElement = lift inputElement
-  -- textAreaElement = lift textAreaElement
-  -- selectElement = lift selectElement
-
-
--- getResponse :: (ToJSON req, FromJSON resp, Reflex t, WithWebSocket t m)
---   => Event t req -> m (Event t resp)
--- getResponse req = do
---   -- Add Event t ByteString for sendEv
---   tell ((:[]) <$> encode <$> req)
---   -- Provide the response
---   ev <- ask
---   return $ fforMaybe ev decode
 
 data Resp = Resp
   deriving (Show, Generic)
