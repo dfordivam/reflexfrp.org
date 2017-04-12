802e125723ee55f6001e09e7be753fe14d295f3f
Divam <dfordivam@gmail.com>
2017-04-12 16:19:08 +0900

    Add fromBS function to get back the data

--- code-snippets/json_dmap.hs
+++ code-snippets/json_dmap.hs
@@ -21,8 +21,11 @@ import Control.Monad.Trans.Control
 import Data.Text
 import Data.List.NonEmpty as NE
 import Data.Dependent.Map
+import Data.Dependent.Sum
 import GHC.Generics
 import Data.Unique.Tag
+import Control.Monad.Primitive
+import Data.Maybe
 
 data Resp = Resp
   deriving (Show, Generic)
@@ -47,6 +50,9 @@ data HasToJSON a where
 data HasFromJSON a where
   HasFromJSON :: (FromJSON (WebSocketResponseType a)) => (WebSocketResponseType a) -> HasFromJSON a
 
+type instance WebSocketResponseType Int = Text
+type instance WebSocketResponseType Text = Bool
+
 main = do
   let addVal m v = do
         t <- newTag
@@ -57,9 +63,29 @@ main = do
 
   let dmapVals = Data.Dependent.Map.toList m2
 
-      b1 = Prelude.map toString dmapVals
+      [b1,b2] = Prelude.map toString dmapVals
   print b1
+  print b2
+
+
+  let v1 = fromBS b1 t1
+      v2 = fromBS b1 t2
+      v3 = fromBS b2 t1
+      v4 = fromBS b2 t2
+
+      val = Prelude.map isJust [v1,v2,v3,v4]
+  print val
   return ()
 
 toString :: DSum t (HasToJSON) -> ByteString
 toString (_ :=> HasToJSON a) = BSL.toStrict $ encode a
+
+fromBS ::
+  (FromJSON (WebSocketResponseType v))
+  => ByteString
+  -> Tag m v
+  -> Maybe (DMap (Tag m) (HasFromJSON))
+fromBS bs t =
+  case decodeStrict bs of
+    Nothing -> Nothing
+    Just v -> Just (Data.Dependent.Map.singleton t (HasFromJSON v))
