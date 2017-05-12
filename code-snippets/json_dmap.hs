982d6577459c2e26e868bdbb1f67bc8f0ba6ec8d
Divam <dfordivam@gmail.com>
2017-05-12 13:09:36 +0900

    Implemented writer monad for encoding

--- code-snippets/json_dmap.hs
+++ code-snippets/json_dmap.hs
@@ -11,7 +11,7 @@
 {-# LANGUAGE ConstraintKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
 
-import Reflex.Dom
+-- import Reflex.Dom
 import Data.Aeson
 import qualified Data.ByteString.Lazy as BSL
 import Data.ByteString
@@ -26,6 +26,9 @@ import GHC.Generics
 import Data.Unique.Tag
 import Control.Monad.Primitive
 import Data.Maybe
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Control.Monad.Writer
 
 data Resp = Resp
   deriving (Show, Generic)
@@ -53,38 +56,59 @@ data HasFromJSON a where
 type instance WebSocketResponseType Int = Text
 type instance WebSocketResponseType Text = Bool
 
+type EncodeM = Writer ([ByteString], Map String (Some (Tag RealWorld )))
+
 main = do
-  let addVal m v = do
-        t <- newTag
-        return (t, Data.Dependent.Map.insert t (HasToJSON v) m)
 
   (t1, m1) <- addVal Data.Dependent.Map.empty ("Voila" :: Text)
   (t2, m2) <- addVal m1 (2::Int)
 
   let dmapVals = Data.Dependent.Map.toList m2
 
-      [b1,b2] = Prelude.map toString dmapVals
-  print b1
-  print b2
+      (_,(bs,mapId)) = runWriter $ mapM_ toString dmapVals
 
+  -- print (t1,b1)
+  -- print (t2,b2)
 
-  let v1 = fromBS b1 t1
-      v2 = fromBS b1 t2
-      v3 = fromBS b2 t1
-      v4 = fromBS b2 t2
+  -- let t1Str = show t1
+  --     --t1' = (read t1Str) -- :: Tag RealWorld HasToJSON
 
-      val = Prelude.map isJust [v1,v2,v3,v4]
-  print val
+  -- let v1 = fromBS b1 t1
+  --     v2 = fromBS b1 t2
+  --     v3 = fromBS b2 t1
+  --     v4 = fromBS b2 t2
+
+  --     val = Prelude.map isJust [v1,v2,v3,v4]
+  -- print val
   return ()
 
-toString :: DSum t (HasToJSON) -> ByteString
-toString (_ :=> HasToJSON a) = BSL.toStrict $ encode a
+addVal m v = do
+  t <- newTag
+  return (t, Data.Dependent.Map.insert t (HasToJSON v) m)
+
+-- getTag :: DMap (Tag RealWorld) (HasFromJSON) -> String -> Maybe (Tag RealWorld v)
+-- getTag dmap str =
+--   where t
+--   filter (\k -> show k == str) (keys dmap)
+
+-- getRequestBS :: Event t (DMap (Tag (PrimState m)) HasToJSON) -> Event t [ByteString]
+-- getRequestBS dmap = t <$> tup1
+--   where
+--     tup1 = (\d -> (keys d, d)) <$> dmap
+--     tup d k = encode (k, d ! k)
+--     t (k, d) -> map (tup d) k
+
+toString ::
+     DSum (Tag RealWorld) (HasToJSON)
+  -> EncodeM ()
+toString (t :=> HasToJSON a) = do
+  tell ([BSL.toStrict $ encode (show t,a)], Map.singleton (show t) (This t))
 
 fromBS ::
   (FromJSON (WebSocketResponseType v))
   => ByteString
-  -> Tag m v
-  -> Maybe (DMap (Tag m) (HasFromJSON))
+  -> Tag RealWorld v
+  -> Maybe (DMap (Tag RealWorld) (HasFromJSON))
 fromBS bs t =
   case decodeStrict bs of
     Nothing -> Nothing
